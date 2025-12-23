5a343f644b841a3f3e50c8e6693a10a56519664f
ajms <albert.schulz@posteo.de>
2025-12-23 08:35:02 +0100

    Add nvim configs

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      json = { "jq" },
+    },
+    formatters = {
+      jq = {
+        command = "jq",
+        args = { "." },
+      },
+    },
+  },
+}
